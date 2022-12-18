60318223f720c50109d7e3fe3c3b3825acccaac6
Teodor Vasilev <silver123@abv.bg>
2022-12-18 14:02:56 +0100

    add react project

---
+++ products-api-react/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class App extends React.Component {
+  render(){
+    return (
+      <div>
+          <h3>Welcome</h3>
+      </div>
+    );
+  }
+}
+
+export default App;
