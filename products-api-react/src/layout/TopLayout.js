a6df31ab7ff442824100138bedb6a4192763d457
Teodor Vasilev <silver123@abv.bg>
2022-12-21 13:38:47 +0100

    update top layout

--- products-api-react/src/layout/TopLayout.js
+++ products-api-react/src/layout/TopLayout.js
@@ -1,6 +1,6 @@
 import './TopLayout.css';
 import React from "react";
-import { PersonFill, HeartFill, CartFill } from "react-bootstrap-icons";
+import { PersonFill, HeartFill, CartFill, EnvelopeAt, Facebook, Whatsapp, Star, StarFill } from "react-bootstrap-icons";
 import { Link } from "react-router-dom";
 
 class TopLayout extends React.Component {
@@ -12,7 +12,37 @@ class TopLayout extends React.Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg bg-light">
+                <div className="container-fluid d-flex justify-content-between review-contacts bg-light">
+                    <div className="review-link">
+                        <Link className='text-decoration-none'>
+                            <div className='d-flex align-items-center'>
+                                <Star className='me-1'></Star>
+                                <span>Client Reviews</span>
+                            </div>
+                        </Link>
+                    </div>
+                    <div className="contacts d-flex">
+                        <Link className='text-decoration-none'>
+                            <div className='contact-item me-4 d-flex align-items-center'>
+                                <EnvelopeAt className='me-1'></EnvelopeAt>
+                                <span>contact@myonlineshop.com</span>
+                            </div>
+                        </Link>
+                        <Link className='text-decoration-none'>
+                            <div className='contact-item d-flex align-items-center'>
+                                <Facebook className='me-1'></Facebook>
+                                <span>MyOnlineShop</span>
+                            </div>
+                        </Link>
+                        <Link className='text-decoration-none'>
+                            <div className='contact-item ms-4 d-flex align-items-center'>
+                                <Whatsapp className='me-1'></Whatsapp>
+                                <span>0898888888</span>
+                            </div>
+                        </Link>
+                    </div>
+                </div>
+                <nav className="navbar navbar-expand-lg">
                     <div className="container-fluid">
                         <div className="me-auto d-flex align-items-center">
                             <a className="navbar-brand logo" href="#"><span className="logo-my">My</span>OnlineShop</a>
@@ -30,28 +60,44 @@ class TopLayout extends React.Component {
                         <div className="collapse navbar-collapse" id="navbarSupportedContent">
                             <div className="ms-auto me-auto">
                                 <form className="d-flex" role="search">
-                                    <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
-                                    <button className="btn btn-outline-primary" type="submit">Search</button>
+                                    <div class="main-search-input-wrap">
+                                        <div class="main-search-input fl-wrap">
+                                            <div class="main-search-input-item">
+                                                <input type="text" placeholder="Search Products..." />
+                                            </div>
+                                            <button class="main-search-button">Search</button>
+                                        </div>
+                                    </div>
+                                    {/* <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
+                                    <button className="btn btn-outline-primary" type="submit">Search</button> */}
                                 </form>
                             </div>
                             <div className="ms-auto">
                                 <ul className="navbar-nav mb-2 mb-lg-0">
                                     <li className="nav-item me-4">
                                         <Link className="nav-link d-flex flex-column align-items-center" to="/login">
-                                        <PersonFill size={28}></PersonFill>
-                                        <span className="nav-link-text">Login</span>
+                                            <PersonFill size={28}></PersonFill>
+                                            <span className="nav-link-text">Login</span>
                                         </Link>
                                     </li>
                                     <li className="nav-item">
-                                        <Link className="nav-link d-flex flex-column align-items-center mt-1">
-                                        <HeartFill size={23}></HeartFill>
-                                        <span className="nav-link-text">Favorites</span>
+                                        <Link className="position-relative nav-link d-flex flex-column align-items-center mt-1">
+                                            <HeartFill size={23}></HeartFill>
+                                            <span className="nav-link-text">Favorites</span>
+                                            <span class="position-absolute top-0 start-50 badge rounded-pill bg-danger">
+                                                2
+                                                <span class="visually-hidden">unread messages</span>
+                                            </span>
                                         </Link>
                                     </li>
                                     <li className="nav-item ms-4">
-                                        <Link className="nav-link d-flex flex-column align-items-center mt-1">
-                                        <CartFill size={23}></CartFill>
-                                        <span className="nav-link-text">Cart</span>
+                                        <Link className="position-relative nav-link d-flex flex-column align-items-center mt-1">
+                                            <CartFill size={23}></CartFill>
+                                            <span className="nav-link-text">Cart</span>
+                                            <span class="position-absolute top-0 start-50 badge rounded-pill bg-danger">
+                                                2
+                                                <span class="visually-hidden">unread messages</span>
+                                            </span>
                                         </Link>
                                     </li>
                                 </ul>
